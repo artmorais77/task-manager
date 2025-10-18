7e1808a1796bbf03cb326300713bea98e22cfa18
Artur Morais <arturmorais.ald@gmail.com>
2025-10-18 01:20:51 -0300

    tratamento de error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
 app.use(express.json());
+app.use(errorHandling)
 
 export { app };
---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,24 @@
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
+
+function errorHandling(
+  error: any,
+  req: Request,
+  res: Response,
+  _: NextFunction
+) {
+  if (error instanceof AppError) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      message: error.message,
+      issues: error.format(),
+    });
+  }
+
+  return res.status(500).json({ message: error.error });
+}
+
+export { errorHandling };
